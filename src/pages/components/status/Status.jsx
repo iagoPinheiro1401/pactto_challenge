0758c0d1af4b951672dc923b68499c47bfd86309
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-08-02 23:07:05 -0300

    home page with welcome and created status

---
+++ src/pages/components/status/Status.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const StyledStatus = styled.div`
+    padding: 3px 7px;
+    border-radius: 10px;
+    background-color: ${props => props.children == 'DONE' ? props.theme.colors.green : 'rgba(252, 252, 252, 0.2)'};
+    color: white;
+    width: 26px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 9px;
+    font-weight: 400;
+`
+
+export default function Status({ children }) {
+    return(
+        <StyledStatus>{children}</StyledStatus>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,61 @@
+import styled from "styled-components"
+
+import TextBig from "./components/tipography/textBig/TextBig"
+import TextMedium from "./components/tipography/textMedium/TextMedium"
+import Status from "./components/status/Status"
+
+const Container = styled.header`
+  display: flex;
+  gap: 40px;
+  flex-direction: column;
+  padding: 40px 30px;
+`
+
+const StatusContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+const StatusAndText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+
+const Hiperlink = styled.a`
+  text-decoration: underline;
+  color: ${props => props.theme.colors.green};
+`
+
 function HomePage () {
   return (
-    <div>
-      home Page
-    </div>
+    <Container>
+        <TextBig>Hey IAGO, welcome to Pactto!</TextBig>
+        <StatusContainer>
+
+          <StatusAndText>
+            <Status>DONE</Status>
+            <TextMedium>Open your inbox and verify your email</TextMedium>
+          </StatusAndText>
+
+          <StatusAndText>
+            <Status>DONE</Status>
+            <TextMedium>Check the <Hiperlink>short video of Pactto in action</Hiperlink></TextMedium>
+          </StatusAndText>
+
+          <StatusAndText>
+            <Status>DONE</Status>
+            <TextMedium>Wanna record your computer screen or review files? Download Pactto for <Hiperlink>Mac</Hiperlink> or <Hiperlink>Windows</Hiperlink></TextMedium>
+          </StatusAndText>
+
+          <StatusAndText>
+            <Status>DONE</Status>
+            <TextMedium>Wanna review videos, images or audio files with your phone or tablet? Download Pactto for <Hiperlink>IOS</Hiperlink> or <Hiperlink>Android</Hiperlink></TextMedium>
+          </StatusAndText>
+        </StatusContainer>
+        
+    </Container>
   )
 }
 
